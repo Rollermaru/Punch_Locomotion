29c95cb2ea67b4c3bb3598384c1e4d3f3a8ff063
Faye Nguyen <32853825+Rollermaru@users.noreply.github.com>
2025-04-28 22:04:34 -0400

    Material Tiling & Adding in my fixed script

--- Assets/Scripts/ActivateTP.cs
+++ Assets/Scripts/ActivateTP.cs
@@ -14,12 +14,14 @@ public class ActivateTP : MonoBehaviour
     [SerializeField] private int frames = 24;
     [SerializeField] private float dist_threshold = 0.3f;   // maybe 0.5 meters?
     public Queue<Vector3> controller_positions;    // Punch activated by comparing first in queue to current position
+    public Queue<Vector3> controllerPositionsLocal; // Local to parent
     public bool activatePunch;      // For other scripts to know that it's time to move
 
     public Vector3 punch_direction; // to tell direction of punch for dashing in punch_direction's direction
 
     public Dash dasher; // To get isDashing boolean to stop checking while moving
     private Transform savedPosition;
+    public Transform floorPosition;
 
     [Header("Flag Proximity")]
     [SerializeField] private Transform[] flags;
@@ -31,25 +33,28 @@ public class ActivateTP : MonoBehaviour
     void Start()
     {
         controller_positions = new Queue<Vector3>(frames);
+        controllerPositionsLocal = new Queue<Vector3>(frames);
         hasLoggedFlag = new bool[flags.Length];
     }
 
     void Update()
     {
         // First handle punch detection
-        if (!dasher.isDashing)
-        {
-            savedPosition = transform;
-            AddPoint(savedPosition);
-            activatePunch = CheckPunch(savedPosition);
+        if (dasher.isDashing) return;   // Don't do checks while dashing
+        if (gameObject.transform.position.y < floorPosition.position.y) return; // Don't do checks under surface
+        
+        savedPosition = transform;
+        AddPoint(savedPosition);
+        activatePunch = CheckPunch(savedPosition);
 
-            if (activatePunch)
-            {
-                punch_direction = Vector3.Normalize( savedPosition.position - controller_positions.Peek());
-                controller_positions.Clear();
-                Debug.Log("PUNCH!!!!");
-            }
+        if (activatePunch)
+        {
+            punch_direction = Vector3.Normalize( savedPosition.position - controller_positions.Peek());
+            controller_positions.Clear();
+            controllerPositionsLocal.Clear();
+            Debug.Log("PUNCH!!!!");
         }
+        
 
         // If dashing is happening, Dash.cs will call DataLogger.LogTeleportStart()
         // So here, we only handle "flag hit" detection logic
@@ -83,9 +88,10 @@ public class ActivateTP : MonoBehaviour
     private bool CheckPunch(Transform trans) 
     {
         if (controller_positions.Count <= 0) return false;
+        if (controllerPositionsLocal.Count <= 0) return false;
 
-        Vector3 then = controller_positions.Peek();
-        Vector3 now = trans.position;
+        Vector3 then = controllerPositionsLocal.Peek();
+        Vector3 now = trans.localPosition;
 
         return (now - then).magnitude > dist_threshold;
     }
@@ -103,7 +109,12 @@ public class ActivateTP : MonoBehaviour
             controller_positions.Dequeue(); // O(1)?
         }
 
+        if (controllerPositionsLocal.Count >= frames - 1) {
+            controllerPositionsLocal.Dequeue();
+        }
+
         controller_positions.Enqueue(trans.position);   // Also O(1) if queue has space
+        controllerPositionsLocal.Enqueue(trans.localPosition);
     }
 
     public Vector3[] GetPoints() {
